e2f6fce8be300b378f02bf0a5165e98628049f40
LisethQuiñones <lisquinonespalacios@gmail.com>
2025-03-18 18:26:04 -0500

    commit2

---
+++ secondExercise.py
@@ -0,0 +1,9 @@
+# Almacenar cadena en variable y mostrar en consola 
+text = "Hola mundo"
+print(text)
+
+
+# preguntar nombre e imprimirlo en pantalla
+name = input("Ingresa tu nombre: ")
+print("Hola",name )
+
